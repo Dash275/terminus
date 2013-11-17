d2275c168bcbdefc0347e3a3c8bfceaed2ef81f7
Eric Hoppe <hoppeeric@gmail.com>
2013-11-16 21:12:31 -0500

    Refactoring

--- color.rb
+++ color.rb
@@ -2,6 +2,4 @@ require "curses"
 
 Curses.start_color
 
-(1..255).each do |color|
-  Curses.init_pair(color, color, Curses::COLOR_BLACK)
-end
+(1..255).each { |color| Curses.init_pair(color, color, Curses::COLOR_BLACK) }

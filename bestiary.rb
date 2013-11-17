e7866437d5bb8027965307ca2da0778807bae99f
Eric Hoppe <hoppeeric@gmail.com>
2013-11-16 19:59:42 -0500

    Figured out colors
    That was annoying. Presumably I won't have to define multiple
    color_pairs for each thing in my program if I use them ad hoc like
    I have in Character.draw, so I can create 1, use 1, close 1 when
    I need color outputs.
    
    Given how long it took me to figure out color_pairs and init_color
    that may wrong though. I do know I had to use 1, and not 0,
    otherwise the color didn't work, but the boldening did. Guess that
    means 0 is the "default" term output color scheme and is hard
    coded.

--- bestiary.rb
+++ bestiary.rb
@@ -1,13 +1,18 @@
 require "curses"
+Curses.start_color # This will eventually go in a more central place
+                   # right after Curses.init_screen.
 
 class Character
   attr_accessor :position
-  attr_accessor :image
+  attr_accessor :image, :color, :style
   attr_accessor :shields, :oxygen
 
   def draw
     Curses.setpos(@position[0], @position[1])
+    Curses.init_pair(1, @color, Curses::COLOR_BLACK)
+    Curses.attron(Curses.color_pair(1)|@style)
     Curses.addstr(@image)
+    Curses.attroff(Curses.color_pair(1)|@style)
     Curses.setpos(@position[0], @position[1])
   end
 
@@ -24,6 +29,8 @@ class Roland < Character
   def initialize(y,x)
     @position = [y,x]
     @image = ?@
+    @color = 46 # True Green
+    @style = Curses::A_BOLD
     @shields = 100
     @oxygen = 100
   end

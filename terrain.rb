c3c0a146979191153aa5d003e57b0cc6db9e7a8e
Eric Hoppe <hoppeeric@gmail.com>
2013-11-18 15:42:04 -0500

    Objects for terrain types

--- terrain.rb
+++ terrain.rb
@@ -3,13 +3,34 @@ require "./entity"
 
 class Terrain < Entity
   attr_accessor :passable
+end
 
-  def initialize(pos, image, color=Curses::COLOR_WHITE, style=Curses::A_NORMAL,
-                 passable=false)
+class Wall < Terrain
+  def initialize(pos, color=237)
     @position = pos
-    @image = image
+    @image = ?#
     @color = color
-    @style = style
-    @passable = passable
+    @style = Curses::A_NORMAL
+    @passable = false
+  end
+end
+
+class Ground < Terrain
+  def initialize(pos, color=237)
+    @position = pos
+    @image = ?.
+    @color = color
+    @style = Curses::A_NORMAL
+    @passable = true
+  end
+end
+
+class Liquid < Terrain
+  def initialize(pos, color=19)
+    @position = pos
+    @image = ?~
+    @color = color
+    @style = Curses::A_NORMAL
+    @passable = true
   end
 end

c1a5c59b3cab56520a54d58e5b24e3b83fe10a85
Eric Hoppe <hoppeeric@gmail.com>
2013-11-17 19:25:08 -0500

    Impassable terrain is possible now
    I have to do the ugly "push a variable through a few unrelated
    functions, but it works.

--- control.rb
+++ control.rb
@@ -3,16 +3,25 @@ require "./message"
 
 module Control
   public
-  def self.feed(char, key)
-    self.move(char, key) if ("yuhjklbn").include?(key)
+  def self.feed(world, char, key)
+    self.move(world, char, key) if ("yuhjklbn").include?(key)
   end
 
   private
-  def self.space_valid?(char, y_move, x_move)
+  def self.space_valid?(world, char, y_move, x_move)
+    floor = []
+    world.floors.each do |f|
+      floor = f if f.beasts.any? {|x| x.class == Roland}
+    end
     new_y = char.position[0] + y_move
     new_x = char.position[1] + x_move
     if space_out_of_bounds?(new_y, new_x)
-      Message.alert("You can't move that way.")
+      Message.alert("Eat the path.")
+      Curses.setpos(char.position[0], char.position[1])
+      return false
+    end
+    if space_blocked?(floor, new_y, new_x)
+      Message.alert("That path is blocked.")
       Curses.setpos(char.position[0], char.position[1])
       return false
     end
@@ -23,7 +32,17 @@ module Control
     return (y < 0 || y > 24 || x < 0 || x > 80) ? true : false
   end
 
-  def self.move(char, key)
+  def self.space_blocked?(floor, y, x)
+    new_pos = [y, x]
+    floor.terrain.each do |entity|
+      if entity.position == new_pos && entity.passable == false
+        return true
+      end
+      return false
+    end
+  end
+
+  def self.move(world, char, key)
     y, x = -1, -1 if key == "y"
     y, x = -1, 1 if key == "u"
     y, x = 0, -1 if key == "h"
@@ -32,7 +51,7 @@ module Control
     y, x = 0, 1 if key == "l"
     y, x = 1, -1 if key == "b"
     y, x = 1, 1 if key == "n"
-    if self.space_valid?(char, y, x)
+    if self.space_valid?(world, char, y, x)
       char.position[0] += y
       char.position[1] += x
     end
--- terrain.rb
+++ terrain.rb
@@ -2,10 +2,14 @@ require "curses"
 require "./entity"
 
 class Terrain < Entity
-  def initialize(pos, image, color=Curses::COLOR_WHITE, style=Curses::A_NORMAL)
+  attr_accessor :passable
+
+  def initialize(pos, image, color=Curses::COLOR_WHITE, style=Curses::A_NORMAL,
+                 passable=false)
     @position = pos
     @image = image
     @color = color
     @style = style
+    @passable = passable
   end
 end
